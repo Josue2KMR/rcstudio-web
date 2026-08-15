cfd7626b267d9ed5c0678a20f1ef431f59b71722
Josué Merino <141958429+Josue2KMR@users.noreply.github.com>
2026-08-14 21:03:22 -0300

    Update sitemap URL in robots.tsx

--- app/robots.tsx
+++ app/robots.tsx
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://rcstudio-web-tori-s.vercel.app/sitemap.xml",
+    sitemap: "https://rcstudio-web-eight.vercel.app/sitemap.xml",
   };
 }
